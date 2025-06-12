9758ea0f26ce744e42917eb13a18d607afdac6d7
Gisel Caicedo <giselcaicedosoler@gmail.com>
2025-06-12 15:27:32 -0500

    Actualización 12/06/25

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apps: [
+    {
+      name: "vc-front",
+      script: "cmd.exe",
+      args: ["/c", "cd /d C:\\Users\\TECNOLOGIA\\Documents\\aplicativos\\vinculacion-clientes\\vc-front && npm run start"],
+      interpreter: "none"
+    }
+  ]
+};
